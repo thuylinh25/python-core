94bdc874ec8919ddd6c75aadfa89277633e8fb31
thuylinh <lethuylinh25@gmail.com>
2020-09-09 17:21:03 +0700

    first commit

---
+++ lesson 4/homework.py
@@ -0,0 +1,5 @@
+# bai tap 00: Viết chương trình sinh list mới bằng cách lấy ngẫu nhiên 5 phần tử từ list gốc
+old_list = [0, 1, 2, 3,4, "abc", 5, 6]
+new_list = [x for x in range(6)]
+print(new_list)
+
