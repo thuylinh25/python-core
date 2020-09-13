95f499e67c12fab52a88fecef3aebb1b2439b875
linh <lethuylinh25@gmail.com>
2020-09-13 23:37:15 +0700

    first commit

--- lesson 4/homework.py
+++ lesson 4/homework.py
@@ -2,18 +2,43 @@
 # import random
 # old_list = [0, 1, 2, 3,4, "abc", 5, 6]
 # new_list =[]
-# while len(new_list) >= 0:
+# for i in range(5):
 #     new_list.append(random.choice(old_list))
-#     if len(new_list) ==5:
-#         print("Chuoi moi la:",new_list)
-#         print("Bai 1 DONE!")
+# print("Chuoi moi la:",new_list)
+
 
 #bai tap 1: Viết chương trình tính tổng, tích của các phần tử trong một list
-my_list = input("Nhap chuoi:")
-tong = 0
-if my_list.isnumeric() == True:
-    print("Nhap dung")
-else:
-    print("Nhap sai")
+# n=int(input("So phan tu cua chuoi la:"))
+# list=[]
+# for i in range (n):
+#     list.append(input())
+# a=0
+# b=1
+# try:
+#     for v in list:
+#         a += int(v)
+#         b *= int(v)
+# except:
+#     a="chuỗi sai"
+#     b="chuỗi sai"
+# print("chuoi la:", list)
+# print ("tong chuoi bang:" +str(a))
+# print ("tich chuoi bang:" +str(b))
 
+#bai tap 2: Viết chương trình tìm số lớn nhất, nhỏ nhất trong một list
+# list=[]
+# n=int(input("So phan tu trong chuoi la:"))
+# for i in range(n):
+#     gt=int(input("gia tri phan tu thu {} la:".format(i)))
+#     list.append(gt)
+# print("chuoi la:", list )
+# if list:
+#     max_list, min_list = list[0], list[0]
+#     for v in list:
+#         if max_list < v:
+#             max_list = v
+#         elif min_list > v:
+#             min_list = v
+#     print(f"so lon nhat la {max_list} va so nho nhat la {min_list}")
 
+#bai tap 3: Viết chương trình tạo ra list mới bằng cách ghép một chuỗi s vào các phần tử list cũ.
