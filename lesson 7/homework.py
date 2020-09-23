6628cd535db234b5883c25830b008f4dbb49d7d2
thuylinh <lethuylinh25@gmail.com>
2020-09-23 17:31:06 +0700

    first commit

---
+++ lesson 7/homework.py
@@ -0,0 +1,12 @@
+# Bài 01: Viết hàm
+#         def max_min(*numbers)
+#     trả lại cả giá trị max, min của nhiều số được truyền vào
+# def max_min(*numbers):
+#     print(max(numbers))
+#     print(min(numbers))
+#     return max(numbers),min(numbers)
+# max_min(2,4,9)
+
+# Bài 02: Viết hàm
+#         def reverse_string(str)
+#     trả lại chuỗi đảo ngược của chuỗi str
