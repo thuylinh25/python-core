8db0cbc14e7c2118d3370f5c829db39ed44909bb
thuylinh <lethuylinh25@gmail.com>
2020-10-29 13:21:13 +0700

    first commit

--- final project/final project 1.py
+++ final project/final project 1.py
@@ -10,7 +10,6 @@
 #     mang tính cá nhân của chính người nhận.
 
 # Gửi đơn
-
 import smtplib, ssl
 port = 465
 sender_email = "shauqia53@gmail.com"
@@ -25,19 +24,28 @@ with smtplib.SMTP_SSL("smtp.gmail.com", port, context = context) as server:
     server.sendmail(sender_email, receiver_email, message.encode("utf-8"))
 
 # Gửi đa
+import openpyxl
+from pathlib import Path
+import pandas as pd
 import csv, smtplib, ssl
 port = 465
 sender_email = "shauqia53@gmail.com"
-receiver_email= "shauqia53+person1@gmail.com"
 message ="""Subject: Greetings
 
 Hi {name}, hope you are well"""
+
 password = input("Type your pasword and press enter: ")
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context = context) as server:
     server.login(sender_email, password)
-    with open("contact_list.csv") as file:
-        reader = csv.reader(file)
-        next(reader)
-        for name, email in reader:
-            server.sendmail(sender_email, email,message.format(name=name).encode("utf-8"))
+
+    file = "excel.xlsx"
+    data = pd.ExcelFile(file)
+    df = data.parse("Sheet1")
+    ps = openpyxl.load_workbook('excel.xlsx')
+    sheet = ps['Sheet1']
+
+    for row in range(2, sheet.max_row + 1):
+        name= sheet['A' + str(row)].value
+        email = sheet['B'+ str(row)].value
+        server.sendmail(sender_email, email, message.format(name=name).encode("utf-8"))
