a6f73efb5091f1dfdf301c46a73fb5263c45e2b4
thuylinh <lethuylinh25@gmail.com>
2020-11-03 12:11:49 +0700

    first commit

--- final project/final project 2.py
+++ final project/final project 2.py
@@ -6,80 +6,80 @@
 #     + Nên xây dựng để mở rộng cho các sản phẩm khác nữa
 #     + Thiết kế Cơ sở dữ liệu để lưu lại các dữ liệu đã trích xuất được
 
-
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import *
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from time import sleep
+from random import randint
 
-webdriver_path = 'C://Users//Linh Le//Downloads//chromedriver.exe'
-Lazadar_url = 'https://www.lazada.vn'
-search_item = 'lò sưởi điện'
+Item_list = np.array(['lò sưởi','máy giặt'])
+for item in Item_list:
+    webdriver_path = 'C://Users//Linh Le//Downloads//chromedriver.exe'
+    browser = webdriver.Chrome(webdriver_path)
+    browser.get('https://www.lazada.vn')
 
-browser = webdriver.Chrome(webdriver_path)
-browser.get(Lazadar_url)
+    search_bar = browser.find_element_by_id('q')
+    search_bar.send_keys(item)
+    search_bar.send_keys(Keys.ENTER)
+    sleep(randint(2, 10))
 
-search_bar = browser.find_element_by_id('q')
-search_bar.send_keys(search_item)
-search_bar.send_keys(Keys.ENTER)
+    item_titles = browser.find_elements_by_class_name('c16H9d')
+    item_prices = browser.find_elements_by_class_name('c13VH6')
 
-item_titles = browser.find_elements_by_class_name('c16H9d')
-item_prices = browser.find_elements_by_class_name('c13VH6')
+    titles_list = []
+    prices_list = []
+    for title in item_titles:
+        titles_list.append(title.text)
+    for price in item_prices:
+        prices_list.append(price.text)
 
-titles_list = []
-prices_list = []
-for title in item_titles:
-    titles_list.append(title.text)
-for price in item_prices:
-    prices_list.append(price.text)
-
-df = pd.DataFrame(zip(titles_list, prices_list), columns=["Tên sản phẩm","Giá"])
-df["Giá"] = df["Giá"].str.replace("₫","")
-df["Giá"] = df["Giá"].str.replace(",","").astype(int)
-df = df[df["Tên sản phẩm"].str.contains("x2") == False]
-df["Trang"] = "Lazada"
-print (df)
-df.to_csv("Giá lò sưởi_lazada .csv")
-
-
-
-webdriver_path = 'C://Users//Linh Le//Downloads//chromedriver.exe'
-Tiki_url = 'https://tiki.vn/'
-search_item = 'máy giặt'
-
-browser = webdriver.Chrome(webdriver_path)
-browser.get(Tiki_url)
+    df = pd.DataFrame(zip(titles_list, prices_list,"Trang"), columns=["Tên sản phẩm","Giá","Lazada"])
+    df["Giá"] = df["Giá"].str.replace("₫","")
+    df["Giá"] = df["Giá"].str.replace(",","").astype(int)
+    df = df[df["Tên sản phẩm"].str.contains("x2") == False]
+    # df["Trang"] = "Lazada"
+    print(df)
+    df.to_csv("Khảo_sát_giá_lazada .csv")
 
 
-search_bar = browser.find_element_by_xpath('//*[@id="__next"]/div[1]/header/div[3]/div/div[1]/div[2]/div/input')
-search_bar.send_keys(search_item)
-search_bar.send_keys(Keys.ENTER)
-
-item_titles = browser.find_elements_by_class_name('title')
-item_prices = browser.find_elements_by_class_name('product-price_current-price')
-
-titles_list = []
-prices_list = []
-for title in item_titles:
-    titles_list.append(title.text)
-for price in item_prices:
-    prices_list.append(price.text)
-
-df = pd.DataFrame(zip(titles_list, prices_list), columns=["Tên sản phẩm","Giá"])
+# webdriver_path = 'C://Users//Linh Le//Downloads//chromedriver.exe'
+# Tiki_url = 'https://tiki.vn/'
+# search_item = 'máy giặt'
+#
+# browser = webdriver.Chrome(webdriver_path)
+# browser.get(Tiki_url)
+#
+#
+# search_bar = browser.find_element_by_xpath('//*[@id="__next"]/div[1]/header/div[3]/div/div[1]/div[2]/div/input')
+# search_bar.send_keys(search_item)
+# search_bar.send_keys(Keys.ENTER)
+#
+# item_titles = browser.find_elements_by_class_name('title')
+# item_prices = browser.find_elements_by_class_name('product-price_current-price')
+#
+# titles_list = []
+# prices_list = []
+# for title in item_titles:
+#     titles_list.append(title.text)
+# for price in item_prices:
+#     prices_list.append(price.text)
+#
+# df = pd.DataFrame(zip(titles_list, prices_list), columns=["Tên sản phẩm","Giá"])
 # df["Giá"] = df["Giá"].str.replace("₫","")
 # df["Giá"] = df["Giá"].str.replace(",","").astype(int)
 # df = df[df["Tên sản phẩm"].str.contains("x2") == False]
-df["Trang"] = "Tiki"
-print(df)
-df.to_csv("Giá lò sưởi_Tiki .csv")
+# df["Trang"] = "Tiki"
+# print(df)
+# df.to_csv("Giá lò sưởi_Tiki .csv")
 
-
-sns.set()
-_ = sns.boxplot(x="Trang", y="Giá", data=df)
-_ = plt.title("So sánh giá lò sưởi giữa các trang thương mại điện tử")
-_ = plt.ylabel("Giá(vnd)")
-_ = plt.xlabel("các trang thương mại điện tử")
-plt.show()
+#
+# sns.set()
+# _ = sns.boxplot(x="Trang", y="Giá", data=df)
+# _ = plt.title("So sánh giá lò sưởi giữa các trang thương mại điện tử")
+# _ = plt.ylabel("Giá(vnd)")
+# _ = plt.xlabel("các trang thương mại điện tử")
+# plt.show()
